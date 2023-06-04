60025ec1c73846360654b48154ea5c823a42c2d1
tsegaye27 <ttalegn29@gmail.com>
2023-06-04 17:20:10 +0300

    created controller for the search internships fxml

--- src/main/java/com/example/internsystemapp/SearchInternshipsController.java
+++ src/main/java/com/example/internsystemapp/SearchInternshipsController.java
@@ -1,2 +1,29 @@
-package com.example.internsystemapp;public class SearchInternshipsController {
+package com.example.internsystemapp;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+import java.io.IOException;
+
+public class SearchInternshipsController {
+
+    @FXML
+    void appliedInternshipsBtnClicked(ActionEvent event) throws IOException {
+        InternApp.showAppliedInternships();
+    }
+
+    @FXML
+    void homeBtnClicked(ActionEvent event) throws IOException{
+
+    }
+
+    @FXML
+    void logoutBtnClicked(ActionEvent event) throws IOException {
+
+    }
+
+    @FXML
+    void searchInternshipsBtnClicked(ActionEvent event) throws IOException {
+        InternApp.showSearchInternships();
+    }
 }
