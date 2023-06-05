44a6f3c211bec9dd1fc4907f71ebf2f3cab47022
tsegaye27 <ttalegn29@gmail.com>
2023-06-05 18:46:59 +0300

    modified the applied internship section's navigation

--- src/main/java/com/example/internsystemapp/AppliedInternshipsController.java
+++ src/main/java/com/example/internsystemapp/AppliedInternshipsController.java
@@ -1,4 +1,41 @@
 package com.example.internsystemapp;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+import java.io.IOException;
+
 public class AppliedInternshipsController {
+    @FXML
+    private Button appliedInternshipsBtn;
+
+    @FXML
+    private Button homeBtn;
+
+    @FXML
+    private Button logoutBtn;
+
+    @FXML
+    private Button searchInternshipsBtn;
+
+    @FXML
+    void appliedInternshipsBtnClicked(ActionEvent event) throws IOException {
+        InternApp.showAppliedInternships();
+    }
+
+    @FXML
+    void homeBtnClicked(ActionEvent event) throws IOException {
+        InternApp.showInternHomePage();
+    }
+
+    @FXML
+    void logoutBtnClicked(ActionEvent event) throws IOException {
+        InternApp.showInternLoginPage();
+    }
+
+    @FXML
+    void searchInternshipsBtnClicked(ActionEvent event) throws IOException {
+        InternApp.showSearchInternships();
+    }
 }
--- src/main/java/com/example/internsystemapp/SearchInternshipsController.java
+++ src/main/java/com/example/internsystemapp/SearchInternshipsController.java
@@ -2,11 +2,22 @@ package com.example.internsystemapp;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 
 import java.io.IOException;
 
 public class SearchInternshipsController {
+    @FXML
+    private Button appliedInternshipsBtn;
 
+    @FXML
+    private Button homeBtn;
+
+    @FXML
+    private Button logoutBtn;
+
+    @FXML
+    private Button searchInternshipsBtn;
     @FXML
     void appliedInternshipsBtnClicked(ActionEvent event) throws IOException {
         InternApp.showAppliedInternships();
@@ -14,12 +25,12 @@ public class SearchInternshipsController {
 
     @FXML
     void homeBtnClicked(ActionEvent event) throws IOException{
-
+        InternApp.showInternHomePage();
     }
 
     @FXML
     void logoutBtnClicked(ActionEvent event) throws IOException {
-
+        InternApp.showInternLoginPage();
     }
 
     @FXML
