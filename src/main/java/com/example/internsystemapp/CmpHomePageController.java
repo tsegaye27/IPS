1ade681b2cc7b23a2e20a4d46f11176af89f6b10
Kaleab Solomon <kaleabslmn@gmail.com>
2023-06-08 04:02:59 +0300

    implemented logout for companies

--- src/main/java/com/example/internsystemapp/CmpHomePageController.java
+++ src/main/java/com/example/internsystemapp/CmpHomePageController.java
@@ -2,7 +2,16 @@ package com.example.internsystemapp;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Optional;
 
 public class CmpHomePageController {
 
@@ -22,11 +31,7 @@ public class CmpHomePageController {
     void homeBtnClicked(ActionEvent event) {
 
     }
-
-    @FXML
-    void logoutBtnClicked(ActionEvent event) {
-
-    }
+    
 
     @FXML
     void manageInternshipsBtnClicked(ActionEvent event) {
@@ -36,6 +41,23 @@ public class CmpHomePageController {
     @FXML
     void postInternshipsBtnClicked(ActionEvent event) {
 
+    }
+    @FXML
+    void logoutBtnClicked(ActionEvent event) throws IOException {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Logout Confirmation");
+        alert.setHeaderText(null);
+        alert.setContentText("Are you sure you want to Logout?");
+        Optional<ButtonType> result = alert.showAndWait();
+        if (result.isPresent() && result.get() == ButtonType.OK){
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("Welcome.fxml"));
+            Parent root = loader.load();
+
+            Stage stage = (Stage) logoutBtn.getScene().getWindow();
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        }
     }
 
 }
