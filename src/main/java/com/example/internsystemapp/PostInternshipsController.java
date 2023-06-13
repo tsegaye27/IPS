42c4f405f9fa772442c7b3013b3ef580822201ff
Kaleab Solomon <kaleabslmn@gmail.com>
2023-06-13 15:38:31 +0300

    can view relevant data for posting internships

--- src/main/java/com/example/internsystemapp/PostInternshipsController.java
+++ src/main/java/com/example/internsystemapp/PostInternshipsController.java
@@ -8,7 +8,10 @@ import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.stage.Stage;
 
+import javax.xml.xpath.XPathEvaluationResult;
 import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Optional;
 
 public class PostInternshipsController {
@@ -54,10 +57,12 @@ public class PostInternshipsController {
     @FXML
     private Button postInternshipsBtn;
 
-    public void initialize(){
+    public void initialize() throws SQLException {
         postInternshipsBtn.setDisable(true);
+        getCompanyDetail();
     }
 
+    String SQL;
     @FXML
     void homeBtnClicked(ActionEvent event) throws IOException {
         postInternshipsBtn.setDisable(false);
@@ -86,4 +91,39 @@ public class PostInternshipsController {
         postInternshipsBtn.setDisable(false);
         InternApp.showManageInternships();
     }
+    private boolean validateInputs(){
+        String title = titleField.getText();
+        String companyName = companyNameField.getText();
+        String location = locationField.getText();
+        String duration = durationField.getText();
+        String contact = contactField.getText();
+        String wage = paidUnpaidField.getText();
+        String vacancies = vacanciesField.getText();
+        String req = requirementsArea.getText();
+        String description = descriptionArea.getText();
+
+        if(title.trim().isEmpty()||companyName.trim().isEmpty()||location.trim().isEmpty()||duration.trim().isEmpty()||contact.trim().isEmpty()||wage.trim().isEmpty()||vacancies.trim().isEmpty()||req.trim().isEmpty()||description.trim().isEmpty()){
+            return false;
+        }
+        return true;
+    }
+
+    private void showError(String message){
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Error");
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
+    void getCompanyDetail() throws SQLException {
+        SQL = "select name, email, location from company where id = "+DBUtills.getCurrentCmpId();
+        System.out.println(SQL);
+        ResultSet rst = DBUtills.getData(SQL);
+        while(rst.next()){
+            companyNameField.setText(rst.getString("name"));
+            contactField.setText(rst.getString("email"));
+            locationField.setText(rst.getString("location"));
+        }
+    }
+
 }
