ae0a6cfed1e65500e2ab444163c2704302b1e3e9
tsegaye27 <ttalegn29@gmail.com>
2023-06-14 00:26:34 +0300

    made some adjustments

--- src/main/java/com/example/internsystemapp/ManageInternshipsController.java
+++ src/main/java/com/example/internsystemapp/ManageInternshipsController.java
@@ -2,9 +2,9 @@ package com.example.internsystemapp;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
-import javafx.scene.control.ButtonType;
+import javafx.scene.control.*;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
 
 import java.io.IOException;
 import java.util.Optional;
@@ -12,7 +12,52 @@ import java.util.Optional;
 public class ManageInternshipsController {
 
     @FXML
+    private Button cancelBtn;
+
+    @FXML
+    private TextField companyNameField;
+
+    @FXML
+    private Label companyNameLabel;
+
+    @FXML
+    private TextField contactField;
+
+    @FXML
+    private Label contactLabel;
+
+    @FXML
+    private Button deleteBtn;
+
+    @FXML
+    private TextArea descriptionArea;
+
+    @FXML
+    private Label descriptionLabel;
+
+    @FXML
+    private TextField durationField;
+
+    @FXML
+    private Label durationLabel;
+
+    @FXML
+    private Button editPostBtn;
+
+    @FXML
     private Button homeBtn;
+
+    @FXML
+    private AnchorPane internshipEditorPane;
+
+    @FXML
+    private VBox internshipsContainer;
+
+    @FXML
+    private TextField locationField;
+
+    @FXML
+    private Label locationLabel;
 
     @FXML
     private Button logoutBtn;
@@ -21,10 +66,80 @@ public class ManageInternshipsController {
     private Button manageInternshipsBtn;
 
     @FXML
+    private AnchorPane managePostPane;
+
+    @FXML
+    private ScrollPane managePostsPane;
+
+    @FXML
+    private Label noInternships;
+
+    @FXML
+    private TextField paidUnpaidField;
+
+    @FXML
+    private Label paidUnpaidLabel;
+
+    @FXML
     private Button postInternshipsBtn;
+
+    @FXML
+    private TextArea requirementsArea;
+
+    @FXML
+    private Label requirementsLabel;
+
+    @FXML
+    private Button returnToViewPostsBtn;
+
+    @FXML
+    private Label searchInternshipTitle;
+
+    @FXML
+    private TextField titleField;
+
+    @FXML
+    private Button updateBtn;
+
+    @FXML
+    private TextField vacanciesField;
+
+    @FXML
+    private Label vacanciesLabel;
+
 
     public void initialize(){
         manageInternshipsBtn.setDisable(true);
+        displayInternships();
+    }
+
+    void displayInternships(){
+
+    }
+
+    @FXML
+    void returnToViewPostsBtnClicked(ActionEvent event){
+
+    }
+
+    @FXML
+    void editPostBtnClicked(ActionEvent event){
+
+    }
+
+    @FXML
+    void deleteBtnClicked(ActionEvent event){
+
+    }
+
+    @FXML
+    void updateBtnClicked(ActionEvent event){
+
+    }
+
+    @FXML
+    void cancelBtnClicked(ActionEvent event){
+
     }
 
     @FXML
