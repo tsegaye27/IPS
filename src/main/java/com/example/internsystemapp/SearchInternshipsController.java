0701df9f84976f8d544aa1cde2a05ecbc613c750
Kaleab Solomon <kaleabslmn@gmail.com>
2023-06-15 02:05:26 +0300

    finalized filtering and searching

--- src/main/java/com/example/internsystemapp/SearchInternshipsController.java
+++ src/main/java/com/example/internsystemapp/SearchInternshipsController.java
@@ -10,6 +10,7 @@ import javafx.scene.layout.VBox;
 import java.io.IOException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Objects;
 import java.util.Optional;
 
 public class SearchInternshipsController {
@@ -132,8 +133,8 @@ public class SearchInternshipsController {
 
     public void initialize() {
         searchInternshipsBtn.setDisable(true);
-        paymentStatusFilterBtn.getItems().add("Paid");
-        paymentStatusFilterBtn.getItems().add("Unpaid");
+        paymentStatusFilterBtn.getItems().add("paid");
+        paymentStatusFilterBtn.getItems().add("unpaid");
         searchResultContainer.setSpacing(10);
         yearOfStudyBox.getItems().add(1);
         yearOfStudyBox.getItems().add(2);
@@ -149,28 +150,33 @@ public class SearchInternshipsController {
         boolean filterByDuration = durationFilterBtn.isSelected();
         boolean filterByField = fieldsFilterBtn.isSelected();
         String selectedValue = paymentStatusFilterBtn.getSelectionModel().getSelectedItem();
+        System.out.println(selectedValue);
         searchResultContainer.getChildren().clear();
 
-        if (searchedText != "") {
+        if (!Objects.equals(searchedText, "")) {
 
+            String paidUnpaidFilter = (selectedValue != null) ? ((selectedValue.equals("unpaid")) ? " and internshipposts.type ='" + selectedValue + "'" : " and internshipposts.type != 'unpaid'") : "";
             if (filterByLocation) {
                 //compare searchedText with location values in DB
-                SQL = "SELECT internshipposts.id FROM internshipposts INNER JOIN company ON internshipposts.company_id = company.id where company.location like '%" + searchedText + "%'";
+                SQL = "SELECT internshipposts.id FROM internshipposts INNER JOIN company ON internshipposts.company_id = company.id where company.location like '%" + searchedText + "%'"+ paidUnpaidFilter;
+                System.out.println(SQL);
             } else if (filterByDuration) {
 
-                SQL = "SELECT id FROM internshipposts where duration like '%" + searchedText + "%'";
+                SQL = "SELECT id FROM internshipposts where duration like '%" + searchedText + "%'"+ paidUnpaidFilter;
+                System.out.println(SQL);
             } else if (filterByField) {
 
-                SQL = "SELECT id,requirements FROM internshipposts where requirements like '%" + searchedText + "%'";
+                SQL = "SELECT id,requirements FROM internshipposts where requirements like '%" + searchedText + "%'"+paidUnpaidFilter;
+                System.out.println(SQL);
 //            SQL="SELECT internshipposts.id, internshipposts.title, internshipposts.duration, internshipposts.requirements, internshipposts.description, internshipposts.type, internshipposts.numberOfApplicantsNeeded, company.name, company.email,company.location FROM internshipposts INNER JOIN company ON internshipposts.company_id = company.id where internshipposts.requirements like '%"+searchedText+"%'";
-
             }
 //        else if(selectedValue.equals("Paid")){
 //                   createHBoxPaid();
 //
 //        }
             else {
-                SQL = "SELECT internshipposts.id FROM internshipposts INNER JOIN company ON internshipposts.company_id = company.id where internshipposts.title like'%" + searchedText + "%' or company.name like '%" + searchedText + "%' or internshipposts.requirements like'%" + searchedText + "%' or internshipposts.duration like '%" + searchedText + "%' or company.location like '%" + searchedText + "%'";
+                SQL = "SELECT internshipposts.id FROM internshipposts INNER JOIN company ON internshipposts.company_id = company.id where internshipposts.title like'%" + searchedText + "%' or company.name like '%" + searchedText + "%' or internshipposts.requirements like'%" + searchedText + "%' or internshipposts.duration like '%" + searchedText + "%' or company.location like '%" + searchedText + "%'"+paidUnpaidFilter;
+                System.out.println(SQL);
             }
 
             ResultSet rst = DBUtills.searchInternships(SQL);
@@ -257,6 +263,7 @@ public class SearchInternshipsController {
             companyNameLabel.setText(rst.getString("name"));
             locationLabel.setText(rst.getString("location"));
             durationLabel.setText(rst.getString("duration"));
+            paidUnpaidLabel.setText(rst.getString("type"));
             contactLabel.setText(rst.getString("email"));
             requirementsLabel.setText(rst.getString("requirements"));
             descriptionLabel.setText(rst.getString("description"));
